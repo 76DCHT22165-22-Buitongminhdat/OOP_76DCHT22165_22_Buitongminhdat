b77b7105c2a81c4b54a39e5466e76757b96b6973
76DCHT22165-22-Buitongminhdat <anhdatut7@gmail.com>
2026-08-18 21:14:37 +0700

    BT01-FINAL: Hoan thanh bai tap
    Updated input prompts for better user guidance and modified output format for employee information display.

--- Baitap01.cpp
+++ Baitap01.cpp
@@ -10,38 +10,46 @@ struct nhanvien {
 typedef struct nhanvien NV;
 
 void nhap(NV &nv){
-	cout <<"nhap ma so nhan vien : ";
+	cout <<"+ Vui long nhap ma so nhan vien : ";
 	cin >> nv.manv;
 	cin.ignore();
-	cout <<"nhap ho va ten :  ";
+	cout <<"+ Vui long nhap ho va ten :  ";
 	getline(cin, nv.tennv);
-	cout <<"nhap muc luong co ban : ";
+	cout <<"+ Vui long nhap muc luong co ban (kVND): ";
 	cin >> nv.luongcoban;
-	cout <<"nhap he so luong : ";
+	cout <<"+ Vui long nhap he so luong : ";
 	cin >> nv.heso;
 }
 
-void xuat(NV nv){
-	cout <<"MaNV : " <<nv.manv
-		 <<" | Ten : " <<nv.tennv
-		 <<" | Luong co ban : " <<nv.luongcoban
-		 <<" | He so : " <<nv.heso <<endl;
+void xuat(int stt, NV nv){
+	cout << stt 
+		<< "\t" << nv.manv 
+		<< "\t\t" << nv.tennv 
+		<< "\t\t"<< nv.luongcoban 
+		<< "\t\t"<< nv.heso << endl;
 }
 int main(){
 	int n;
-	cout <<"nhap so luong nhan vien : ";
-	cin >> n;
-	
-	nhanvien *ds = new nhanvien[n];
+	cout <<"-----------------------------------------------"<<endl;	
+	cout <<"    CHUONG TRING QUAN LY THONG TIN NHAN VIEN"<<endl;
+	cout <<"-----------------------------------------------"<<endl;
+	cout <<"Chao ban! Xin vui long nhap so luong nhan vien : ";
+	cin  >> n;
+	cout <<"-----------------------------------------------"<<endl;
+	NV *ds = new NV[n];
 	
 	for(int i = 0; i < n; i++ ){
-		cout <<"Nhap thong tin cho nhan vien thu "<<i+1<<": "<<endl;
+		cout <<"[->] Nhap thong tin cho nhan vien thu "<<i+1<<endl;
 		nhap(ds[i]);
+		cout <<"-----------------------------------------------"<<endl;
 	}
-	cout <<"\n--- DANH SACH NHAN VIEN---"<<endl;
-	for(int i = 0 ; i<n; i++){
-		xuat(ds[i]);
-	}
+	cout << "\n---DANH SACH NHAN VIEN---" << endl;
+	cout << "-----------------------------------------------------------------------" << endl;
+    cout << "STT\tMa NV\t\tHo va Ten\t\tLuong CB\tHe so" << endl;
+    cout << "-----------------------------------------------------------------------" << endl;
+    for(int i = 0; i < n; i++) {
+        xuat(i+1, ds[i]); 
+    }
 	delete[] ds;
 	return 0;
 }
